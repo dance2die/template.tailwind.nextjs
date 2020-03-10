dd8b0b83294c8d9689ff6d8ad21d1bba86b5c51d
dance2die <sungmkim80@gmail.com>
2020-03-09 21:27:27 -0400

    Added comment for postcss.config.js difference

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,10 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
+// Unlike in create-react-app, you can't use `require`.
+// Reference: https://github.com/zeit/next.js/issues/10117
+// Workaround: https://github.com/zeit/next.js/issues/10117#issuecomment-574892000
+// PostCSS-Loader documentation: https://github.com/postcss/postcss-loader#configuration
 module.exports = {
   plugins: {
     "tailwindcss": {},
