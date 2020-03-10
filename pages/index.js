230405289008bb0154e074053b9ab6e18f1a46a1
Sung M. Kim <sungmkim80@gmail.com>
2020-03-10 19:46:01 -0400

    Added body content

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 const Home = () => (
-  <div className="container">
+  <div>
     <Head>
       <title>Create Next App + TailWind</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel='icon' href='/favicon.ico' />
     </Head>
 
-    <main className="w-screen">
-      <header>
-        <h2 className="bg-pink-500 font-serif font-bold text-white p-4 text-2xl">
+    <main className='w-screen h-screen flex flex-col'>
+      <header className='text-center'>
+        <h2 className='w-full bg-pink-500 font-serif font-bold text-white p-8 text-3xl'>
           ~Welcome to Next.js 9.3.0 + Tailwind CSS~
         </h2>
       </header>
-      <section>
-        body content
+      <section className='flex-1 flex justify-center items-center'>
+        <h2 className='text-6xl'>This is the Body Content</h2>
       </section>
     </main>
   </div>
-);
+)
 
-export default Home;
+export default Home
